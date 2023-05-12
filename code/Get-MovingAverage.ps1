539560aff2d4eafd3d9def30eb55a9fc68cb7fa6
voytas <wnapierala@udt.gov.pl>
2023-05-12 23:58:52 +0200

    knowledge

--- code/Get-MovingAverage.ps1
+++ code/Get-MovingAverage.ps1
@@ -16,3 +16,12 @@ function Get-MovingAverage {
     # Output moving average
     Write-Output "The $period-period moving average for $symbol is $ma_value."
 }
+
+<# 
+# Define historical price data
+$prices = @(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# Call function to calculate 5-period moving average
+Get-MovingAverage -symbol "TEST" -prices $prices -period 5
+
+#>
