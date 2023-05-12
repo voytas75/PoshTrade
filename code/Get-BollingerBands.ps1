9b46f11d4bd4ecc152672bb2b1b662a89b4d0d4e
voytas <wnapierala@udt.gov.pl>
2023-05-13 00:06:11 +0200

    bollinger

--- code/Get-BollingerBands.ps1
+++ code/Get-BollingerBands.ps1
@@ -23,3 +23,23 @@ function Get-BollingerBands {
     # Output Bollinger Bands
     Write-Output "The Bollinger Bands for $symbol are: Upper Band = $upper_band, Lower Band = $lower_band."
 }
+
+<# 
+# Example usage
+$symbol = "BTC/USD"
+$prices = @(
+    [pscustomobject]@{ date = "2022-01-01"; close = 50000 },
+    [pscustomobject]@{ date = "2022-01-02"; close = 55000 },
+    [pscustomobject]@{ date = "2022-01-03"; close = 52000 },
+    [pscustomobject]@{ date = "2022-01-04"; close = 48000 },
+    [pscustomobject]@{ date = "2022-01-05"; close = 49000 },
+    [pscustomobject]@{ date = "2022-01-06"; close = 51000 },
+    [pscustomobject]@{ date = "2022-01-07"; close = 53000 },
+    [pscustomobject]@{ date = "2022-01-08"; close = 56000 },
+    [pscustomobject]@{ date = "2022-01-09"; close = 59000 },
+    [pscustomobject]@{ date = "2022-01-10"; close = 61000 }
+)
+
+Get-BollingerBands -symbol $symbol -prices $prices -period 20 -num_std_devs 2
+
+#>
