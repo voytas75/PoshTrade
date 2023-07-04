a22277f19d82c25c0135f35993ebe256adba1455
Wojciech Napierała <wnapierala@hotmail.com>
2023-07-04 22:52:14 +0200

    new deam, ema

--- code/Get-DEMA.ps1
+++ code/Get-DEMA.ps1
@@ -1,21 +1,56 @@
 function Get-DEMA {
+    <#
+.SYNOPSIS
+    Calculates the Double Exponential Moving Average (DEMA) for a given array of data points.
+
+.DESCRIPTION
+    The Get-DEMA function calculates the Double Exponential Moving Average (DEMA) for a given array of data points.
+    It uses the Get-EMA function to calculate the exponential moving averages and then applies the DEMA formula to obtain the DEMA values.
+
+.PARAMETER DataPoints
+    The array of data points for which the DEMA needs to be calculated.
+    The data points should be provided as an array of doubles.
+
+.PARAMETER Period
+    The period or number of data points to consider for the calculation of DEMA.
+    It should be a positive integer value.
+
+.EXAMPLE
+    Get-DEMA -DataPoints 1, 2, 3, 4, 5 -Period 3
+    Calculates the DEMA for the data points [1, 2, 3, 4, 5] using a period of 3.
+
+.NOTES
+    Author: Wojciech Napierała
+    Date: 04.07.2023
+#>
+
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
         [double[]]$DataPoints,
+
         [Parameter(Mandatory = $true)]
+        [ValidateRange(1, [int]::MaxValue)]
         [int]$Period
     )
 
-    $ema1 = Get-EMA -DataPoints $DataPoints -Period $Period
-    $ema2 = Get-EMA -DataPoints $ema1 -Period $Period
-    $demaValues = New-Object 'System.Collections.Generic.List[Double]'
-    for ($i = 0; $i -lt $DataPoints.Length; $i++) {
-        $dema = 2 * $ema1[$i] - $ema2[$i]
-        $demaValues.Add($dema)
-    }
+    try {
+        $ema1 = Get-EMA -DataPoints $DataPoints -Period $Period
+        $ema2 = Get-EMA -DataPoints $ema1 -Period $Period
 
-    return $demaValues
+        $demaValues = @()
+
+        for ($i = 0; $i -lt $DataPoints.Length; $i++) {
+            $dema = 2 * $ema1[$i] - $ema2[$i]
+            $demaValues += $dema
+        }
+
+        return $demaValues
+    }
+    catch {
+        Write-Error "An error occurred while calculating DEMA: $_"
+    }
 }
 
 <# 
--- code/Get-EMA.ps1
+++ code/Get-EMA.ps1
@@ -1,21 +1,64 @@
 function Get-EMA {
+    <#
+    .SYNOPSIS
+    Calculates the Exponential Moving Average (EMA) for a given set of data points.
+    
+    .DESCRIPTION
+    This function calculates the Exponential Moving Average (EMA) for a given set of data points.
+    The EMA is a commonly used technical indicator used in financial analysis.
+    
+    .PARAMETER DataPoints
+    An array of doubles representing the data points for which the EMA needs to be calculated.
+    
+    .PARAMETER Period
+    An integer representing the period or number of data points to consider in the EMA calculation.
+    
+    .EXAMPLE
+    PS> $data = 1..10 | foreach { $_ + Get-Random -Minimum 1 -Maximum 10 }
+    PS> $ema = Get-EMA -DataPoints $data -Period 5
+    PS> $ema
+    2.5
+    3.25
+    4.125
+    5.0625
+    6.03125
+    6.515625
+    6.7578125
+    6.87890625
+    6.939453125
+    6.9697265625
+    
+    .NOTES
+    Author: Wojciech Napierała
+    Date: 04.07.2023
+    Version: 1.1
+    #>
+    
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
         [double[]]$DataPoints,
+
         [Parameter(Mandatory = $true)]
+        [ValidateRange(1, [int]::MaxValue)]
         [int]$Period
     )
 
-    $multiplier = 2 / ($Period + 1)
-    $emaValues = New-Object 'System.Collections.Generic.List[Double]'
-    $emaValues.Add($DataPoints[0])
-    for ($i = 1; $i -lt $DataPoints.Length; $i++) {
-        $ema = ($DataPoints[$i] - $emaValues[$i - 1]) * $multiplier + $emaValues[$i - 1]
-        $emaValues.Add($ema)
-    }
+    try {
+        $multiplier = 2 / ($Period + 1)
+        $emaValues = New-Object 'System.Collections.Generic.List[Double]'
+        $emaValues.Add($DataPoints[0])
+        for ($i = 1; $i -lt $DataPoints.Length; $i++) {
+            $ema = ($DataPoints[$i] - $emaValues[$i - 1]) * $multiplier + $emaValues[$i - 1]
+            $emaValues.Add($ema)
+        }
 
-    return $emaValues
+        return $emaValues
+    }
+    catch {
+        Write-Error "An error occurred while calculating the EMA. Details: $($_.Exception.Message)"
+    }
 }
 
 <# 
