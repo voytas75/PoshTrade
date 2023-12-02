f796f19c149df60260d85066d8824e461c6d2d6f
Voytas <voytass@op.pl>
2023-12-02 12:17:20 +0100

    Update Get-OBV.ps1
    Update

--- code/Get-OBV.ps1
+++ code/Get-OBV.ps1
@@ -1,3 +1,18 @@
+<#
+.SYNOPSIS
+    Calculates the On-Balance Volume (OBV) for a given set of closing prices and corresponding volumes.
+.DESCRIPTION
+    This script defines a PowerShell function, Get-OBV, which calculates the OBV values based on the provided closing prices and volumes.
+.PARAMETER Close
+    An array of closing prices.
+.PARAMETER Volume
+    An array of corresponding volumes.
+.EXAMPLE
+    $closePrices = 1.1, 1.2, 1.3, 1.2, 1.1
+    $volumes = 100, 200, 300, 200, 100
+    Get-OBV -Close $closePrices -Volume $volumes
+#>
+
 function Get-OBV {
     [CmdletBinding()]
     param (
@@ -7,32 +22,27 @@ function Get-OBV {
         [double[]] $Volume
     )
 
+    # Create a list to store On-Balance Volume (OBV) values
     $obv = [System.Collections.Generic.List[Double]]::new()
-    $obv.Add($Volume[0])
+    $obv.Add($Volume[0])  # Initialize with the first volume value
 
+    # Calculate OBV values
     for ($i = 1; $i -lt $Close.Length; $i++) {
         $prevClose = $Close[$i - 1]
         $currentClose = $Close[$i]
         $currentVolume = $Volume[$i]
 
+        # Determine whether to add, subtract, or maintain OBV value
         if ($currentClose -gt $prevClose) {
-            $obv.Add($obv[$i - 1] + $currentVolume)
+            $obv.Add($obv[$i - 1] + $currentVolume)  # Price increased
         }
         elseif ($currentClose -lt $prevClose) {
-            $obv.Add($obv[$i - 1] - $currentVolume)
+            $obv.Add($obv[$i - 1] - $currentVolume)  # Price decreased
         }
         else {
-            $obv.Add($obv[$i - 1])
+            $obv.Add($obv[$i - 1])  # Price unchanged
         }
     }
 
     return $obv
 }
-
-<# 
-$closePrices = 1.1, 1.2, 1.3, 1.2, 1.1
-$volumes = 100, 200, 300, 200, 100
-
-Get-OBV -Close $closePrices -Volume $volumes
-
-#>
