16fb9a5fb4e830695e2d93102061401432c14fcd
Voytas <voytass@op.pl>
2023-12-02 14:29:19 +0100

    Update Get-FibonacciRetracement.ps1

--- code/Get-FibonacciRetracement.ps1
+++ code/Get-FibonacciRetracement.ps1
@@ -1,3 +1,21 @@
+<#
+.SYNOPSIS
+   Calculate Fibonacci retracement levels based on the provided StartPrice and EndPrice.
+
+.DESCRIPTION
+   This function calculates Fibonacci retracement levels for a given price range.
+
+.PARAMETER StartPrice
+   The starting price for the Fibonacci retracement calculation.
+
+.PARAMETER EndPrice
+   The ending price for the Fibonacci retracement calculation.
+
+.EXAMPLE
+   The following example demonstrates how to use the function:
+   Get-FibonacciRetracement -StartPrice 100 -EndPrice 200
+#>
+
 function Get-FibonacciRetracement {
     [CmdletBinding()]
     param(
@@ -7,17 +25,13 @@ function Get-FibonacciRetracement {
         [double]$EndPrice
     )
 
+    # Fibonacci retracement levels calculation
     $retracementLevels = @()
-    $retracementLevels += ($EndPrice - ($EndPrice - $StartPrice) * 0.236)
-    $retracementLevels += ($EndPrice - ($EndPrice - $StartPrice) * 0.382)
-    $retracementLevels += ($EndPrice - ($EndPrice - $StartPrice) * 0.5)
-    $retracementLevels += ($EndPrice - ($EndPrice - $StartPrice) * 0.618)
-    $retracementLevels += ($EndPrice - ($EndPrice - $StartPrice) * 0.786)
+    $retracementLevels += ($EndPrice - ($EndPrice - $StartPrice) * 0.236)  # 23.6% retracement level
+    $retracementLevels += ($EndPrice - ($EndPrice - $StartPrice) * 0.382)  # 38.2% retracement level
+    $retracementLevels += ($EndPrice - ($EndPrice - $StartPrice) * 0.5)    # 50% retracement level
+    $retracementLevels += ($EndPrice - ($EndPrice - $StartPrice) * 0.618)  # 61.8% retracement level
+    $retracementLevels += ($EndPrice - ($EndPrice - $StartPrice) * 0.786)  # 78.6% retracement level
 
     return $retracementLevels
 }
-
-
-<# 
-Get-FibonacciRetracement -StartPrice 100 -EndPrice 200
-#>
