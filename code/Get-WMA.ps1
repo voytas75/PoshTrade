9c46f37928c57e11aae6560c3704e4421e8c96f6
Wojciech Napierała <voytass@op.pl>
2023-12-02 23:46:31 +0100

    The changes made in this commit include: - Modified Get-WMA.ps1 to add a function that calculates the weighted moving average of a series of data points. The function takes two parameters: DataPoints and Period. It returns a list of the weighted moving average values.

--- code/Get-WMA.ps1
+++ code/Get-WMA.ps1
@@ -1,3 +1,22 @@
+<# 
+.SYNOPSIS
+Calculate the weighted moving average of a series of data points.
+
+.DESCRIPTION
+The Get-WeightedMovingAverage function calculates the weighted moving average for a given series of data points and a specified period. It returns a list of the weighted moving average values.
+
+.PARAMETER DataPoints
+The array of data points for which to calculate the weighted moving average.
+
+.PARAMETER Period
+The period over which to calculate the weighted moving average.
+
+.EXAMPLE
+$dataPoints = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+Get-WeightedMovingAverage -DataPoints $dataPoints -Period 3
+# Returns: 2, 3, 4, 5, 6, 7, 8, 9
+
+#>
 function Get-WeightedMovingAverage {
     [CmdletBinding()]
     param(
@@ -7,22 +26,24 @@ function Get-WeightedMovingAverage {
         [int]$Period
     )
 
+    # Create a new list to store the weighted moving average values
     $wmaValues = New-Object 'System.Collections.Generic.List[Double]'
+    
+    # Calculate the weighted moving average for each data point
     for ($i = 0; $i -lt ($DataPoints.Length - $Period + 1); $i++) {
         $sum = 0.0
         $divisor = 0.0
+        
+        # Calculate the sum and divisor for the weighted average
         for ($j = $i; $j -lt ($i + $Period); $j++) {
             $sum += ($DataPoints[$j] * ($Period - ($j - $i)))
             $divisor += ($Period - ($j - $i))
         }
+        
+        # Add the weighted average value to the list
         $wmaValues.Add($sum / $divisor)
     }
 
+    # Return the list of weighted moving average values
     return $wmaValues
 }
-
-<# 
-$dataPoints = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-Get-WeightedMovingAverage -DataPoints $dataPoints -Period 3
-
-#>
